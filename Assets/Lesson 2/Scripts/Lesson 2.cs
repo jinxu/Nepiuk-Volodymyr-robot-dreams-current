fa3110b9997f51f76939c25452cf82cfa86c4466
Volodymyr Nepiuk <jinxu@jinxu-pro.local>
2025-01-23 22:46:47 -0800

    Initial commit

---
+++ Assets/Lesson 2/Scripts/Lesson 2.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Lesson2 : MonoBehaviour
+{
+    private List<float> _floatList;
+    [SerializeField] private float _floatX;
+
+    [ContextMenu("Add to List")]
+    private void HelloWorld(){
+        _floatList.Add(_floatX);        
+    }
+
+    [ContextMenu("Remove from List")]
+    private void RemoveFromList(){
+        _floatList.Remove(_floatX);
+    }
+
+    [ContextMenu("Clear List")]
+    private void ClearList(){
+        _floatList.Clear();
+    }
+
+    [ContextMenu("Sort List")]
+    private void SortList(){
+        _floatList.Sort();
+    }
+
+    [ContextMenu("Reverse List")]
+    private void ReverseList(){
+        _floatList.Reverse();
+    }
+
+    [ContextMenu("Print List")]
+    private void PrintList(){
+        Debug.Log(string.Join("\n", _floatList));
+    }
+}
+
